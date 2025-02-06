abb839717473d51780752ab515ef08e51dfa446d
thongphst <facebookmailst@gmail.com>
2025-02-07 03:43:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -147,13 +147,13 @@ func main() {
 				bot.Send(helpMsg)
 
 			case "connect":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hãy nhập chuỗi kết nối database.")
-				bot.Send(msg)
+				//msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hãy nhập chuỗi kết nối database.")
+				//bot.Send(msg)
 
-				update = <-updates // Get the next update (user input)
+				//update = <-updates // Get the next update (user input)
 
-				connStr := update.Message.Text
-				//connStr := postgresql://neondb_owner:npg_mNH1XnzoLTQ6@ep-raspy-shape-a8fyxfop-pooler.eastus2.azure.neon.tech/neondb?sslmode=require			
+				//connStr := update.Message.Text
+				connStr :="postgresql://neondb_owner:npg_mNH1XnzoLTQ6@ep-raspy-shape-a8fyxfop-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 				if connStr == "" {
 					errMsg := tgbotapi.NewMessage(update.Message.Chat.ID, "Chuỗi kết nối không hợp lệ.")
 					bot.Send(errMsg)
